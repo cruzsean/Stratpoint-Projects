62837d80f0b004b9e77e363c472ead44e90b4d49
Sean Cruz <seanfredrick2004@gmail.com>
2024-06-26 10:24:22 +0800

    Added Test Cases

---
+++ mini-project-2/src/test/java/com/SeanCruz/BookTest.java
@@ -0,0 +1,24 @@
+package com.SeanCruz;
+
+import junit.framework.TestCase;
+
+public class BookTest extends TestCase {
+
+    public void testBookConstructorAndGetters() {
+        Book book = new Book("Test Book", "Test Author", "1234567890");
+        assertEquals("Test Book", book.getTitle());
+        assertEquals("Test Author", book.getAuthor());
+        assertEquals("1234567890", book.getISBN());
+    }
+
+    public void testBookSetters() {
+        Book book = new Book("Test Book", "Test Author", "1234567890");
+        book.setTitle("New Title");
+        book.setAuthor("New Author");
+        book.setISBN("0987654321");
+        assertEquals("New Title", book.getTitle());
+        assertEquals("New Author", book.getAuthor());
+        assertEquals("0987654321", book.getISBN());
+    }
+
+}
